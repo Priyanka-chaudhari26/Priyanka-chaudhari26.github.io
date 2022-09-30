b3e09f8cfa65145b62e1875585cbbd8f0b13733c
Priyanka-chaudhari26 <90328159+Priyanka-chaudhari26@users.noreply.github.com>
2022-09-30 20:58:56 +0530

    Create App.js

---
+++ instapayment/src/App.js
@@ -0,0 +1,20 @@
+import Navigationbar from "./Components/Navigationbar";
+import DonationPage from "./Components/DonationPage";
+import Footer from "./Components/Footer";
+import PaymentBtn from './Components/PaymentButton';
+import About from "./Components/About";
+
+
+function App() {
+  return (
+    <>
+    <Navigationbar/>
+    <DonationPage/>
+    <About/>
+    <PaymentBtn/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
